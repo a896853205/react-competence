b2babcaca76977c6f35473f65e1d1f1bce6594bb
Eric <896853205@qq.com>
2022-01-10 17:40:13 +0800

    feat: The JSX configuration is complete

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,27 @@
 import typescript from "@rollup/plugin-typescript";
+import resolve from "rollup-plugin-node-resolve";
+import jsx from 'acorn-jsx';
 
 export default {
   input: "src/index.ts",
+  external: ["react", "react-dom"],
+  globals: {
+    react: "React",
+    "react-dom": "ReactDOM",
+  },
   output: {
     file: "dist/bundle.cjs.js",
     format: "cjs",
     name: "react-competence",
   },
+  acornInjectPlugins: [jsx()],
   plugins: [
     typescript({
       lib: ["es5", "es6"],
       target: "es5",
-			exclude: 'node_modules/**'
+      exclude: "node_modules/**",
+      jsx: "preserve",
     }),
+    resolve(),
   ],
 };
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import a from "./example-tsx";
+
+console.log(a);
